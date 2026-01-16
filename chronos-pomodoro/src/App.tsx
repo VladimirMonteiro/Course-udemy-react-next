3e3028b3eb337e04450eeb36afc69d314d5657ea
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-01-16 18:58:28 -0300

    Feat: Add logo

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,21 +1,28 @@
 import './styles/theme.css';
 import './styles/global.css';
+
 import { TimerIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
 
 function App() {
   return (
-    <Container>
-      <Heading>
-        <>
-          Chronos Pomodoro
-          <button>
-            <TimerIcon />
-          </button>
-        </>
-      </Heading>
-    </Container>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Heading>
+          <>
+            Chronos Pomodoro
+            <button>
+              <TimerIcon />
+            </button>
+          </>
+        </Heading>
+      </Container>
+    </>
   );
 }
 
