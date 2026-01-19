014a2eaa95505cfa52fcb5f81fe4e2b72cbf2903
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-01-19 18:24:02 -0300

    Feat: add form

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -5,6 +5,10 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { Button } from './components/Button';
+import { PlayCircleIcon } from 'lucide-react';
 
 function App() {
   return (
@@ -17,6 +21,22 @@ function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaultInput id='task' type='text' placeholder='Digite algo' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <Cycles />
+          </div>
+          <div className='formRow'>
+            <Button icon={<PlayCircleIcon />} color='red' />
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ chronos-pomodoro/src/components/DefaultInput/index.tsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.css';
+
+type DefaultInputProps = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<'input'>;
+
+export function DefaultInput({
+  labelText,
+  id,
+  type,
+  ...rest
+}: DefaultInputProps) {
+  return (
+    <>
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input
+        className={styles.input}
+        type={type}
+        name='task'
+        id={id}
+        {...rest}
+      />
+    </>
+  );
+}
