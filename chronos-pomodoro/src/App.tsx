94a804a9642b94d706079bdaac2d4bb79c31d679
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-01-20 17:10:47 -0300

    Feat: add context

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -2,32 +2,14 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
-import type { TaskStateModel } from './models/TaskStateModel';
-import { useState } from 'react';
-
-//export type TaskStateModel = {
-//  tasks: TaskModel[];
-//  secondsRemaining: number;
-//  activeTask: TaskModel | null;
-//  currentCycle: number; // 1 a 8
-//  config: {
-//    workTime: number;
-//   shortWorkTime: number;
-//   longBreakTime: number;
-// };
-//};
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formatedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: { workTime: 25, shortBreakTime: 5, longBreakTime: 15 },
-};
+import { TaskProvider } from './contexts/TaskContext/TaskProvider';
 
 function App() {
-  return <Home />;
+  return (
+    <TaskProvider>
+      <Home />;
+    </TaskProvider>
+  );
 }
 
 export { App };
--- chronos-pomodoro/src/pages/Home/index.tsx
+++ chronos-pomodoro/src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
 import { MainTemplate } from '../../components/templates/MainTemplate';
+import { TaskContext } from '../../contexts/TaskContext/TaskContext';
 
 export function Home() {
+  const taskContext = useContext(TaskContext);
+  console.log(taskContext);
   return (
     <MainTemplate>
       <Container>
