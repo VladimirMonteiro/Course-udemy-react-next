1c48d967ef2996638d81f26d8a6607a4860c0b86
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2026-01-21 15:15:54 -0300

    Feat: add create task

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -4,12 +4,64 @@ import { Cycles } from '../Cycles';
 import { DefaultInput } from '../DefaultInput';
 
 import styles from './styles.module.css';
+import { useRef, useState } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(state.currentCycle);
+
+  const handleCreateNewTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const taskName = taskNameInput.current?.value.trim();
+
+    if (!taskName) {
+      alert('Por favor, preencha o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formatedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleCreateNewTask} className={styles.form}>
       <div className={styles.formRow}>
-        <DefaultInput id='task' type='text' placeholder='Digite algo' />
+        <DefaultInput
+          id='task'
+          type='text'
+          placeholder='Digite algo'
+          ref={taskNameInput}
+        />
       </div>
       <div className={styles.formRow}>
         <p>Lorem ipsum dolor sit amet.</p>
